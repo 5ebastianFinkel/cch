4400c078629220c0a86905cd0f4103a42e3c19e2
Sebastian Finkel <s.finkel@posteo.de>
2022-12-05 15:58:59 +0100

    first challenge with test setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# cch
-Code Challange Hour
+# Code Challange Hour
+
+## Die Regeln:
+
+* Es wird immer im Pair (Team) programmiert
+* Aufgabe sind Time-boxed oder beendet wenn ein Team eine Loesung hat
+* Aufgabe sind aus dem Bereich js, css, vue
---
+++ src/sessions/session1/team1/c1.js
@@ -0,0 +1,28 @@
+// Challenge Number 1
+//
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+//
+// For example, if we run 9119 through the function, 811181 will come out, because 9ˆ2 is 81 and 1ˆ2 is 1.
+//
+// Note: The function accepts an integer and returns an integer
+//
+function squareDigits(num) {
+    return 0;
+}
+
+// in-source test suites
+if (import.meta.vitest) {
+    const {it, expect} = import.meta.vitest
+
+    it("squareDigits(3212) should equal 9414", () => {
+        expect(squareDigits(3212)).toBe(9414);
+    })
+
+    it("squareDigits(2112) should equal 4114", () => {
+        expect(squareDigits(2112)).toBe(4114);
+    });
+
+    it("squareDigits(0) should equal 0", () => {
+        expect(squareDigits(0)).toBe(0);
+    });
+}
