839d66a738f534091776669efd8f202e45f15713
Sebastian Finkel <s.finkel@posteo.de>
2022-12-06 13:13:07 +0100

    add missing vite and vue files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Team1Card from "./sessions/session1/team1/Challenge3/Team1Card.vue";
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+  <team1-card/>
+</template>
+
+<style scoped>
+</style>
